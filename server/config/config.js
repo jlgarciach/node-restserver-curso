d017edf3c04154310adbebb940d0c629040fd8f9
jlgarciach <56977476+jlgarciach@users.noreply.github.com>
2020-09-01 14:49:53 -0600

    Nuevo commit de acceso MONGODB

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = process.env.MONGO_URI;
+    urlDB = process.env.MONGO_URI; // agrague variable de acceso
 }
 
 process.env.URLDB = urlDB;
